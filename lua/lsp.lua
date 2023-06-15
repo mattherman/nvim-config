d2bfac1519f422dc185a1c071c1816c6bff13a31
Matthew Herman <mattherman11@gmail.com>
2023-06-14 19:46:45 -0500

    Added lua-language-server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,29 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.ccls.setup {
-    on_attach = lsp_on_attach
+lspconfig.ccls.setup{}
+
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'lua5.3'
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'}
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false
+      }
+    }
+  }
 }
 
 -- Use LspAttach autocommand to only map the following keys
